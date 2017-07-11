355830264917d88cb284f56a9ad3f805f73de117
Ayo Isaiah <sudo@ayoisaiah.com>
2017-07-11 19:34:18 +0100

    Compile sass files and use autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
 const babelify = require('babelify');
 const browserify = require('browserify');
 const vinylSource = require('vinyl-source-stream');
@@ -8,12 +10,13 @@ const glob = require('glob');
 
 gulp.task('copyStaticFiles', () => gulp.src([
   './src/**/*.*',
-  '!./src/js/*.js',
+  '!./src/js/**/*.js',
+  '!./src/sass/**/*.sass',
 ])
   .pipe(gulp.dest('./dist')));
 
 gulp.task('build', (done) => {
-  glob('./src/js/*.js', (err, files) => {
+  glob('./src/js/**/*.js', (err, files) => {
     if (err) done(err);
     const tasks = files.map(entry => browserify({
       entries: [entry],
@@ -27,7 +30,13 @@ gulp.task('build', (done) => {
   });
 });
 
+gulp.task('sass', () => gulp.src('./src/sass/main.sass')
+  .pipe(sass())
+  .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+  .pipe(gulp.dest('./dist/css')));
+
 gulp.task('default', () => {
   gulp.watch('./src/js/**/*.js', ['build']);
-  gulp.watch(['./src/**/*.*', '!./src/js/*.js'], ['copyStaticFiles']);
+  gulp.watch(['./src/**/*.*', '!./src/js/**/*.js', '!./src/sass/**/*.sass'], ['copyStaticFiles']);
+  gulp.watch(['./src/sass/**/*.sass'], ['sass']);
 });
