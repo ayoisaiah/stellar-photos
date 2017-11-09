5cd8f60c187f8292041422f1dd310ecdcc3c37a3
Ayo Isaiah <sudo@ayoisaiah.com>
2017-11-09 15:59:28 +0100

    Show more info about the background image

--- src/js/components/info-popover.js
+++ src/js/components/info-popover.js
@@ -7,21 +7,30 @@ const infoPopover = (nextImage, fullDate) => `
     <button class="control-button info-button">
       <svg class="icon icon-info"><use href="#icon-info"></use></svg>
     </button> 
+
     <ul class="popover-content">
+
       <li class="popover-content-item">
-        <a href="${nextImage.user.links.html}?utm_source=stellar-photos&utm_medium=referral&utm_campaign=api-credi" class="photographer-info" target="_blank" rel="noreferrer">
-          <img src="${nextImage.user.profile_image.small}" class="photographer-dp" />
-          <span class="photographer-name">${nextImage.user.first_name || ''} ${nextImage.user.last_name || ''}</span>
+        <a href="${nextImage.user.links.html}?utm_source=stellar-photos&utm_medium=referral&utm_campaign=api-credit" 
+        class="photographer-info" target="_blank" rel="noreferrer">
+          <img src="${nextImage.user.profile_image.small}" 
+            class="photographer-dp" />
+          <span class="photographer-name">
+            ${nextImage.user.first_name || ''} ${nextImage.user.last_name || ''}
+          </span>
         </a>
       </li>
+
       <li class="popover-content-item">
         <span class="label">Resolution</span>
         <span class="resolution">${nextImage.width} x ${nextImage.height}</span>
       </li>
+
       <li class="popover-content-item">
         <span class="label">Created On</span>
         <span class="created-date">${fullDate}</span>
       </li>
+
       <li class="popover-content-item">
         <span class="label">Likes</span>
         <div class="wrapper">
@@ -29,9 +38,45 @@ const infoPopover = (nextImage, fullDate) => `
           <svg class="icon heart-icon"><use href="#icon-heart"></use></svg>
         </div>
       </li>
+
+      <li class="popover-content-item">
+        <span class="label">Downloads</span>
+        <div class="wrapper">
+          <span class="downloads">${Number(nextImage.downloads)
+    .toLocaleString()}</span>
+          <svg class="icon download-icon">
+            <use href="#icon-download"></use>
+          </svg>
+        </div>
+      </li>
+
+      <li class="popover-content-item">
+        <span class="label">Views</span>
+        <div class="wrapper">
+          <span class="views">${Number(nextImage.views)
+    .toLocaleString()}</span>
+          <svg class="icon eye-icon"><use href="#icon-eye"></use></svg>
+        </div>
+      </li>
+
+      <li class="popover-content-item">
+        <span class="label">Camera Model</span>
+        <div class="wrapper">
+          <span class="camera-model">${nextImage.exif.model}</span>
+          <svg class="icon camera-icon"><use href="#icon-camera"></use></svg>
+        </div>
+      </li>
+
+      <li class="popover-content-item">
+        <span class="label">Camera Make</span>
+        <span class="camera-make">${nextImage.exif.make}</span>
+      </li>
+
       <li class="popover-content-item">
         <span class="label"><a href="${nextImage.links.html}?utm_source=stellar-photos&utm_medium=referral&utm_campaign=api-credit" class="linkToPhoto" target="_blank" rel="noreferrer">View photo on Unsplash.com</a></span>
       </li>
+
+
     </ul>
   </div>
 `;
