5911f70aade68666edcd8d1c0cad7a273778ed5b
Ayo Isaiah <sudo@ayoisaiah.com>
2018-01-26 06:06:27 +0100

    Move event listeners to separate file

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
-import { chainableClassList, $ } from './libs/helpers';
+import { $ } from './libs/helpers';
 import purify from './libs/purify-dom';
+import eventListeners from './libs/event-listeners';
 import { initializeSearch } from './modules/search';
 import initializeHistory from './modules/history';
 import initializeWeather from './modules/weather';
@@ -9,7 +10,6 @@ import main from './components/main';
 import loader from './components/loader';
 import footer from './components/footer';
 import header from './components/header';
-
 
 const body = $('app');
 body.insertAdjacentHTML('afterbegin', purify.sanitize(`
@@ -29,52 +29,4 @@ initializeSearch();
 
 initializeWeather();
 
-const uiElements = document.querySelectorAll('.s-ui');
-
-const showControls = () => {
-  uiElements.forEach(element => chainableClassList(element).remove('hide-ui'));
-};
-
-const hideControls = () => {
-  // Check if one popover is active
-  const popovers = Array.from(document.querySelectorAll('.popover-content'));
-  const arePopoversOpen = popovers.some(e => e.classList
-    .contains('popover-content--is-visible'));
-
-  // Don't hide controls if any popover is open
-  if (arePopoversOpen) return;
-
-  // Also don't hide controls if history pane is open
-  const historyPane = $('s-history');
-  if (historyPane.classList.contains('open')) return;
-
-  // Also don't hide controls if there is an active search
-  const searchResults = $('searchResults');
-  if (searchResults.hasChildNodes()) return;
-
-  uiElements.forEach(element => chainableClassList(element).add('hide-ui'));
-};
-
-showControls();
-
-// Hide the buttons and bars after 2 seconds of inactivity
-
-let timeout = setTimeout(() => hideControls(), 2000);
-
-uiElements.forEach((element) => {
-  element.addEventListener('mouseenter', () => {
-    showControls();
-    if (timeout) clearTimeout(timeout);
-  });
-
-  element.addEventListener('mouseleave', () => {
-    timeout = setTimeout(() => hideControls(), 2000);
-  });
-});
-
-// Close all popovers when click is detected outside
-document.addEventListener('click', (node) => {
-  if (node.target.matches('.popover *')) return;
-  const popover = document.querySelectorAll('.popover .popover-content');
-  popover.forEach(e => e.classList.remove('popover-content--is-visible'));
-});
+eventListeners();
