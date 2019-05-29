2370b7623540f3b1abfb9f800ab66bf2a0b13b4d
Ayo Isaiah <ayisaiah@gmail.com>
2019-05-29 08:42:56 +0100

    Fix eslint errors

--- src/js/libs/helpers.js
+++ src/js/libs/helpers.js
@@ -1,21 +1,21 @@
 const convertTimeStamp = timestamp => {
-  const date = new Date(timestamp * 1000),
-    days = [
-      'Sunday',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-    ],
-    currentDay = days[date.getDay()],
-    year = date.getFullYear(),
-    month = `0${date.getMonth() + 1}`.slice(-2),
-    day = `0${date.getDate()}`.slice(-2),
-    hours = `0${date.getHours()}`.slice(-2),
-    minutes = `0${date.getMinutes()}`.slice(-2),
-    seconds = `0${date.getSeconds()}`.slice(-2);
+  const date = new Date(timestamp * 1000);
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const currentDay = days[date.getDay()];
+  const year = date.getFullYear();
+  const month = `0${date.getMonth() + 1}`.slice(-2);
+  const day = `0${date.getDate()}`.slice(-2);
+  const hours = `0${date.getHours()}`.slice(-2);
+  const minutes = `0${date.getMinutes()}`.slice(-2);
+  const seconds = `0${date.getSeconds()}`.slice(-2);
 
   return {
     currentDay,
