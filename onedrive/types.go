df27a95446fb4d7d40871d70648e44265b38a4d8
Ayo Isaiah <ayisaiah@gmail.com>
2019-05-31 04:13:50 +0100

    Fix wrong type in Onedrive response

--- onedrive/types.go
+++ onedrive/types.go
@@ -6,9 +6,8 @@ type OnedriveId struct {
 
 type OnedriveAuth struct {
 	Token_type    string `json:"token_type"`
-	Expires_in    string `json:"expires_in"`
+	Expires_in    int    `json:"expires_in"`
 	Scope         string `json:"scope"`
 	Access_token  string `json:"access_token"`
 	Refresh_token string `json:"refresh_token"`
 }
-
