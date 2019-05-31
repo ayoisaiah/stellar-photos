37978802d4bcaa841697d16c99ac1ecccde0fc42
Ayo Isaiah <ayisaiah@gmail.com>
2019-05-31 04:35:11 +0100

    Update test image urls

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,14 +8,13 @@ var imageUrls = []struct {
 	url    string
 	result bool
 }{
-	// {"https://freshman.tech/assets/dist/images/vim-javascript/featured-400.jpg", true},
+	{"https://freshman.tech/assets/dist/images/vim-javascript/featured-400.jpg", true},
 	{"https://freshman.tech/assets/dist/images/articles/freshman-1600.png", false},
 }
 
 func TestImageUrlToBase64(t *testing.T) {
 	for _, value := range imageUrls {
 		str, err := ImageUrlToBase64(value.url)
-		fmt.Println(err)
 		if value.result == true {
 			if str == "" {
 				t.Errorf("Expected result, got empty string")
