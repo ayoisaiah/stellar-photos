38facf342badf5fc656250ad9731400978ae6a41
Ayooluwa Isaiah <ayisaiah@gmail.com>
2020-12-30 22:47:44 +0100

    Ensure save url job is completed before responding

--- dropbox/dropbox.go
+++ dropbox/dropbox.go
@@ -18,6 +18,11 @@ type key struct {
 	DropboxKey string `json:"dropbox_key"`
 }
 
+type SaveURLResponse struct {
+	Tag        string `json:".tag"`
+	AsyncJobID string `json:"async_job_id"`
+}
+
 // SendDropboxKey sends the application key to the client on request to avoid
 // exposing it in the extension code
 func SendDropboxKey(w http.ResponseWriter, r *http.Request) error {
@@ -33,6 +38,58 @@ func SendDropboxKey(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	return utils.JsonResponse(w, bytes)
+}
+
+func checkJobStatus(jobId string, token string) error {
+	v := fmt.Sprintf("Bearer %s", token)
+	requestBody, err := json.Marshal(map[string]string{
+		"async_job_id": jobId,
+	})
+	if err != nil {
+		return err
+	}
+
+	endpoint := "https://api.dropboxapi.com/2/files/save_url/check_job_status"
+
+	request, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", v)
+
+	response, err := utils.Client.Do(request)
+	if err != nil {
+		if os.IsTimeout(err) {
+			return utils.NewHTTPError(err, http.StatusRequestTimeout, "Request to external API timed out")
+		}
+
+		return err
+	}
+
+	defer response.Body.Close()
+
+	b, err := utils.CheckForErrors(response)
+	if err != nil {
+		return err
+	}
+
+	resp := &SaveURLResponse{}
+
+	err = json.Unmarshal(b, resp)
+	if err != nil {
+		return err
+	}
+
+	if resp.Tag == "complete" {
+		return nil
+	} else if resp.Tag == "in_progress" {
+		time.Sleep(1 * time.Second)
+		return checkJobStatus(jobId, token)
+	}
+
+	return fmt.Errorf("Job failed. Response from Dropbox: %s", string(b))
 }
 
 // SaveToDropbox saves the requested photo to the current user's Dropbox account
@@ -71,8 +128,7 @@ func SaveToDropbox(w http.ResponseWriter, r *http.Request) error {
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("Authorization", v)
 
-	client := &http.Client{Timeout: 10 * time.Second}
-	response, err := client.Do(request)
+	response, err := utils.Client.Do(request)
 	if err != nil {
 		if os.IsTimeout(err) {
 			return utils.NewHTTPError(err, http.StatusRequestTimeout, "Request to external API timed out")
@@ -83,11 +139,30 @@ func SaveToDropbox(w http.ResponseWriter, r *http.Request) error {
 
 	defer response.Body.Close()
 
-	_, err = utils.CheckForErrors(response)
+	b, err := utils.CheckForErrors(response)
 	if err != nil {
 		return err
 	}
 
-	w.WriteHeader(http.StatusOK)
-	return nil
+	resp := &SaveURLResponse{}
+
+	err = json.Unmarshal(b, resp)
+	if err != nil {
+		return err
+	}
+
+	if resp.Tag == "async_job_id" {
+		err = checkJobStatus(resp.AsyncJobID, token)
+		if err != nil {
+			return err
+		}
+
+		w.WriteHeader(http.StatusOK)
+		return nil
+	} else if resp.Tag == "complete" {
+		w.WriteHeader(http.StatusOK)
+		return nil
+	}
+
+	return fmt.Errorf("Save URL error. Response from Dropbox: %s", string(b))
 }
