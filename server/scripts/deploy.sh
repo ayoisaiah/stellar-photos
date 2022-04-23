b1f074a2a8038731692591d615394e0127d182e3
Ayooluwa Isaiah <ayisaiah@gmail.com>
2022-04-24 00:39:51 +0100

    Update server deploy script

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+# change working directory to script directory
+cd "$(dirname "$0")"
 
 # exit when any command fails
 set -e
 
 echo "Pulling latest docker image"
 docker pull ayoisaiah/stellar-photos-server:latest
-docker container inspect stellar-photos-server > /dev/null 2>&1 || docker stop stellar-photos-server && docker rm -f stellar-photos-server
+
+if docker container inspect stellar-photos-server > /dev/null 2>&1; then
+  docker stop stellar-photos-server && docker rm -f stellar-photos-server
+fi
+
 docker volume create cached_images
 docker run --env-file .env -d -p 8080:8080 -v cached_images:/app/cached_images --name stellar-photos-server ayoisaiah/stellar-photos-server:latest
 docker system prune -af
